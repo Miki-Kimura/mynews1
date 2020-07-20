8e2d5dcf21eaf2af3dcd18441cf9bb1ca24a8eb1
miki <shooting0girl@gmail.com>
2020-07-20 12:41:31 +0000

    heroku

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\HTML;
+
+use App\News;
 
 class HomeController extends Controller
 {
+    
+    public function index(Request $request)
+    {
+        $posts = News::all()->sortByDesc('updated_at');
+        
+        if (count($posts) >0) {
+            $headline = $posts->shift();
+        } else {
+            $headline = null;
+        }
+        
+        // news/index.blade.php ファイルを渡している
+        // また　Viewテンプレートに headline、posts、という変数を渡している
+        return view('news.index', ['headline' => $headline, 'posts' => $posts]);
+    }
     /**
      * Create a new controller instance.
      *
--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index')->name('hello');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-     Route::get('news/create', 'Admin\NewsController@add');
-     Route::post('news/create', 'Admin\NewsController@create'); # 追記
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
+    Route::get('news','Admin\NewsController@index');
      
-     Route::get('profile/create', 'Admin\ProfileController@add');
-     Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+    
 });
