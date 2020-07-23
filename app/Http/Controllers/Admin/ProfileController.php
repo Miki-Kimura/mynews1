51064feddddcaa73e1198e01240d3c514ecb6a94
miki <shooting0girl@gmail.com>
2020-07-23 02:55:14 +0000

    　13課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -14,6 +14,25 @@ class ProfileController extends Controller
     }
     public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+      
+        $profile = new Profile;
+        $form = $request->all();
+
+        // formに画像があれば、保存する
+        if ($form['image']) {
+            $path = $request->file('image')->store('public/image');
+            $profile->image_path = basename($path);
+        } else {
+            $profile->image_path = null;
+        }
+
+        unset($form['_token']);
+        unset($form['image']);
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
         return redirect('admin/profile/create');
     }
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>プロフィール新規作成</h2>
-                <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
                         <ul>
@@ -18,41 +18,27 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
+                        <label class="col-md-2">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
                     
-                    //以下貼り付け
+                    <!--以下貼り付け-->
                      <div class="form-group row">
-                        <label class="col-md-2" for="name">氏名</label>
+                        <label class="col-md-2" for="name">性別</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
-                        </div>
-                    </div>
-                    
-                     <div class="form-group row">
-                        <label class="col-md-2" for="gender">性別</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
+                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
                         </div>
                     </div>
                     
                      <div class="form-group row">
                         <label class="col-md-2" for="hobby">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
                         </div>
                     </div>
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2" for="introduction">自己紹介欄</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ $news_form->body }}</textarea>
-                        </div>
-                    </div>
-                    //貼り付け終了
+                
                     
                     <div class="form-group row">
                         <label class="col-md-2">本文</label>
