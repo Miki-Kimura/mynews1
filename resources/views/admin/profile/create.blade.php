781c35022df1aed0898286f76a6bed1a2da9255e
miki <shooting0girl@gmail.com>
2020-08-08 03:22:42 +0000

    laravel13課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -43,7 +43,7 @@
                     <div class="form-group row">
                         <label class="col-md-2">自己紹介</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="name" rows="5">{{ old('name') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="5">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
