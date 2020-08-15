2d5ae15523f26f6b02368965c8776ea93ca9161f
miki <shooting0girl@gmail.com>
2020-08-15 03:02:56 +0000

    PHPLaravel課題の途中

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -37,13 +37,48 @@ class ProfileController extends Controller
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+    public function edit(Request $request)
     {
-        //
+        $profile = Profile::find($request->id);
+        // dd($profile);
+        if (empty($profile)) {
+            abort(404);    
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
-    public function update()
+    public function update(Request $request)
     {
-        //
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // News Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+      
+        if ($request->remove == 'ture') {
+            $profile_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $profile_form['image_path'] = basename($path);
+        } else {
+            $profile_form['image_path'] = $profile->image_path;
+        }
+      
+        unset($profile_form['_token']);
+      
+        unset($profile_form['image']);
+      
+        unset($profile_form['remove']);
+      
+        $profile->fill($profile_form)->save();
+    
+        $history = new ProfileHistory;
+        $history->profile_id = $profile->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+      
+        return redirect('admin/profile/edit?id=' . $profile->id);
+
     }
 }
